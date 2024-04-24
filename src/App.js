cec1d94f4cf1b97e3c1bee94918d876363c92214
jhoancanchila <canchila.jhoan@gmail.com>
2024-04-24 17:28:34 -0500

    add component cta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
+import CTA from "./components/CTA";
 
 function App() {
   return (
     <Fragment>
       <Sidebar/>
+      <CTA/>
     </Fragment>
   );
 }
